2ec8ee8b99594765273a36ef256fb6e1785ec110
abhijeet91100 <abhijeetkumar0970@gmail.com>
2026-01-19 17:37:37 +0530

    Add files via upload

---
+++ LargestOfTwo.java
@@ -0,0 +1,12 @@
+public class LargestOfTwo {
+    public static void main(String args[]) {
+        int A = 1;
+        int B = 5;
+        if(A >= B) {
+            System.out.println("A is the largest of 2");
+        }
+       else {
+        System.out.println("B is the largest of 2");
+        }
+    }
+}
---
+++ ScannerObjectAddition.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ public class ScannerObjectAddition {
+  public static void main(String args[]){
+  Scanner sc = new Scanner(System.in);
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+  int addition = a + b;
+  System.out.println(addition);
+  }
+ }
